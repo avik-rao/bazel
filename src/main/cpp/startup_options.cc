4a629e3c9dedd2f1a88d83941988d134e9461e1d
Alessandro Patti <ale812@yahoo.it>
2024-08-22 05:21:22 -0700

    Add cgroup startup option
    This options will have the server start in the user-specifed cgroup. This is useful when using the cgroup features in bazel and thus the server must be started in a user-writable cgroup.
    
    Closes #21618.
    
    PiperOrigin-RevId: 666305298
    Change-Id: Ide3bb3f65bd11f4ecc11d6e843926a16ecb41f2f

--- src/main/cpp/startup_options.cc
+++ src/main/cpp/startup_options.cc
@@ -97,6 +97,9 @@ StartupOptions::StartupOptions(const string &product_name,
       macos_qos_class(QOS_CLASS_UNSPECIFIED),
 #endif
       unlimit_coredumps(false),
+#ifdef __linux__
+      cgroup_parent(),
+#endif
       windows_enable_symlinks(false) {
   if (blaze::IsRunningWithinTest()) {
     output_root = blaze_util::MakeAbsolute(blaze::GetPathEnv("TEST_TMPDIR"));
@@ -165,6 +168,7 @@ StartupOptions::StartupOptions(const string &product_name,
   RegisterUnaryStartupFlag("output_user_root");
   RegisterUnaryStartupFlag("server_jvm_out");
   RegisterUnaryStartupFlag("failure_detail_out");
+  RegisterUnaryStartupFlag("experimental_cgroup_parent");
 }
 
 StartupOptions::~StartupOptions() {}
@@ -386,6 +390,12 @@ blaze_exit_code::ExitCode StartupOptions::ProcessArg(
           "multiple times.";
       return blaze_exit_code::BAD_ARGV;
     }
+  } else if ((value = GetUnaryOption(
+                  arg, next_arg, "--experimental_cgroup_parent")) != nullptr) {
+#ifdef __linux__
+    cgroup_parent = value;
+    option_sources["cgroup_parent"] = rcfile;
+#endif
   } else {
     bool extra_argument_processed;
     blaze_exit_code::ExitCode process_extra_arg_exit_code = ProcessArgExtra(
